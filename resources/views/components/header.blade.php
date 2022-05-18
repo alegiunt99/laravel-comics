7f2df39ea3ba04fa773497495dbc7914a03b9f77
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-18 18:22:50 +0200

    Ho modificato lo stile dell'header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,16 +9,16 @@
 
       <!-- il div della barra di navigazione -->
       <div class="nav-bar">
-        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
-        <div class="nav-link"><a href="#"></a>COMICS</div>
-        <div class="nav-link"><a href="#"></a>MOVIES</div>
-        <div class="nav-link"><a href="#"></a>TV</div>
-        <div class="nav-link"><a href="#"></a>GAMES</div>
-        <div class="nav-link"><a href="#"></a>COLLECTIBLES</div>
-        <div class="nav-link"><a href="#"></a>VIDEOS</div>
-        <div class="nav-link"><a href="#"></a>FANS</div>
-        <div class="nav-link"><a href="#"></a>NEWS</div>
-        <div class="nav-link"><a href="#"></a>SHOP</div>
+        <div class="nav-link"><a href="/">CHARACTERS</a></div>
+        <div class="nav-link"><a href="/">COMICS</a></div>
+        <div class="nav-link"><a href="/">MOVIES</a></div>
+        <div class="nav-link"><a href="/">TV</a></div>
+        <div class="nav-link"><a href="/">GAMES</a></div>
+        <div class="nav-link"><a href="/">COLLECTIBLES</a></div>
+        <div class="nav-link"><a href="/">VIDEOS</a></div>
+        <div class="nav-link"><a href="/">FANS</a></div>
+        <div class="nav-link"><a href="/">NEWS</a></div>
+        <div class="nav-link"><a href="/">SHOP</a></div>
       </div>
 
     </div>
