fb7248f288ce9d42503ccf313af8597a265f0ea1
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-18 18:01:31 +0200

    Ho creato il layout base e ho inserito l'header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,26 @@
+<header class="header-page">
+    <!-- creo la classe container che centra il contenuto-->
+    <div class="container">
+
+      <!-- il div del logo -->
+      <div class="logo">
+        <img src="images/dc-logo.png" alt="Logo DC-Comics">
+      </div>
+
+      <!-- il div della barra di navigazione -->
+      <div class="nav-bar">
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>COMICS</div>
+        <div class="nav-link"><a href="#"></a>MOVIES</div>
+        <div class="nav-link"><a href="#"></a>TV</div>
+        <div class="nav-link"><a href="#"></a>GAMES</div>
+        <div class="nav-link"><a href="#"></a>COLLECTIBLES</div>
+        <div class="nav-link"><a href="#"></a>VIDEOS</div>
+        <div class="nav-link"><a href="#"></a>FANS</div>
+        <div class="nav-link"><a href="#"></a>NEWS</div>
+        <div class="nav-link"><a href="#"></a>SHOP</div>
+      </div>
+
+    </div>
+    
+  </header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Comics - Home</title>
-</head>
-<body>
-    <h1>Home</h1>
-</body>
-</html>
+@extends('layout.app')
+
+@section('title')
+    DC Comics - Home
+@endsection
+
+@section('content')
+    <ul>
+        <li><a href="/">Ciao</a></li>
+        <li><a href="/">Ciao</a></li>
+        <li><a href="/">Ciao</a></li>
+        <li><a href="/">Ciao</a></li>
+        <li><a href="/">Ciao</a></li>
+        <li><a href="/">Ciao</a></li>
+        <li><a href="/">Ciao</a></li>
+    </ul>
+@endsection
