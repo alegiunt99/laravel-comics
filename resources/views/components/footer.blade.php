7f0794a574cd5e89c3e82e9e44c3bae9a2a6428a
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-18 18:43:28 +0200

    Ho inserito anche il footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,99 @@
+<footer>
+
+    <!-- footer centrale con i link per le altre pagine -->
+    <div id="links-footer">
+      <div class="container">
+        <div class="links-box">
+          <div class="links-item">
+            <p>DC COMICS</p>
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+          </div>
+          <div class="links-item">
+            <p>DC</p>
+            <ul>
+              <li><a href="#">Terms of Use</a></li>
+              <li><a href="#">Privacy policy (new)</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertisings</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent Workshop</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact us</a></li>
+            </ul>
+          </div>
+          <div class="links-item">
+            <p>SITES</p>
+            <ul>
+              <li><a href="#">DC </a></li>
+              <li><a href="#">MAD</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </div>
+
+          <div class="links-item">
+            <p>SHOP</p>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Footer inferiore con i link per i social network -->
+    <div id="bottom-footer">
+      <div class="container">
+        <div class="subsribe-button">
+          <button>SIGN UP NOW</button>
+        </div>
+
+        <div class="social-networks">
+          <div class="follow-us">
+            <p>FOLLOW US</p>
+          </div>
+
+          <div class="social-icons">
+
+            <div class="social-button">
+              <img src="../images/footer-facebook.png" alt="">
+            </div>
+            
+            <div class="social-button">
+              <img src="../images/footer-twitter.png" alt="">
+            </div>
+            
+            <div class="social-button">
+              <img src="../images/footer-youtube.png" alt="">
+            </div>
+
+            <div class="social-button">
+              <img src="../images/footer-pinterest.png" alt="">
+            </div>
+
+            <div class="social-button">
+              <img src="../images/footer-periscope.png" alt="">
+            </div>
+        
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    
+  </footer>
